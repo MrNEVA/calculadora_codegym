838a05948d042c8c5aa573edd7a2e7592b63f288
Mr.NEVA <mrnevart@gmail.com>
2022-11-11 13:17:03 -0400

    añadi función de suma

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('LA SUPER CALCULADORA')
+function comenzar() {
+console.log('calculadora corriendo...')
+}
+function suma(a, b) {
+    return a + b
+}
+
+const resultadoSuma = suma(9, 8)
+
+console.log(resultadoSuma)
+
+comenzar()
